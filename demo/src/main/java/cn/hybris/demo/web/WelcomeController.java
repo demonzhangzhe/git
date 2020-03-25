b4757a7c8cf0d439de260d5ac0d3a15c300715bd
DESKTOP-211LEUK\Administrator <349663408@qq.com>
2020-03-25 12:22:46 +0800

    111

---
+++ demo/src/main/java/cn/hybris/demo/web/WelcomeController.java
@@ -0,0 +1,21 @@
+package cn.hybris.demo.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ *
+ */
+@RestController
+public class WelcomeController
+{
+	/**
+	 * @return Hello world
+	 */
+	@RequestMapping("/welcome")
+	public String welcome()
+	{
+		return "Hello world";
+	}
+}
